281d34b4d5edd4dc812de5e6954541ea92e56564
KONDO, Hiroyasu <hrknd@users.noreply.github.com>
2021-08-09 20:20:54 +0900

    increment/decrement with hooks

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import react from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(count+1)
+  const decrement = () => setCount(count-1)
+  const increment2 = () => setCount((previousCount) => previousCount+1)
+  const decrement2 = () => setCount((previousCount) => previousCount-1)
+  const resetcount = () => setCount(0)
+  const x2count = () => setCount(previousCount => previousCount *2)
+  const x3count = () => setCount(previousCount => {
+    if((previousCount % 3) == 0) {
+      return previousCount / 3
+    }
+  })
+  
   return (
+    <>
     <div>
-      Helo.
+      count:{count}
     </div>
+    <div>
+    <button onClick={increment}>+1</button>
+    <button onClick={decrement}>-1</button>
+    </div>
+    <div>
+    <button onClick={increment2}>+1</button>
+    <button onClick={decrement2}>-1</button>
+    </div>
+    <div>
+    <button onClick={resetcount}>Reset</button>
+    <button onClick={x2count}>x2</button>
+    <button onClick={x3count}>x3</button>
+    </div>
+    </>
   );
 }
 
