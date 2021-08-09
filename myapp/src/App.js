27ff0984607d43c7420f498d2aecf190eb73d74b
KONDO, Hiroyasu <hrknd@users.noreply.github.com>
2021-08-09 20:40:05 +0900

    multi state hooks

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,41 +1,28 @@
 import react from 'react';
 import React, {useState} from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = (props) => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment = () => setCount(count+1)
-  const decrement = () => setCount(count-1)
-  const increment2 = () => setCount((previousCount) => previousCount+1)
-  const decrement2 = () => setCount((previousCount) => previousCount-1)
-  const resetcount = () => setCount(0)
-  const x2count = () => setCount(previousCount => previousCount *2)
-  const x3count = () => setCount(previousCount => {
-    if((previousCount % 3) == 0) {
-      return previousCount / 3
-    }
-  })
-  
+  const reset = () =>{
+    setPrice(props.price)
+    setName(props.name)
+  }
   return (
     <>
-    <div>
-      count:{count}
-    </div>
-    <div>
-    <button onClick={increment}>+1</button>
-    <button onClick={decrement}>-1</button>
-    </div>
-    <div>
-    <button onClick={increment2}>+1</button>
-    <button onClick={decrement2}>-1</button>
-    </div>
-    <div>
-    <button onClick={resetcount}>Reset</button>
-    <button onClick={x2count}>x2</button>
-    <button onClick={x3count}>x3</button>
-    </div>
+    <p>現在の{name}は、{price}円です。</p>
+    <button onClick={() => setPrice(price + 1)}>+1</button>
+    <button onClick={() => setPrice(price - 1)}>-1</button>
+    <button onClick={reset}>Reset</button>
+    <input value={name} onChange={e => setName(e.target.value)}/>
     </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App;
