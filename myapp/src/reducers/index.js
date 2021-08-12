5f4b3742acc2196949c7644fecb3e31591fe8066
KONDO, Hiroyasu <hrknd@users.noreply.github.com>
2021-08-12 22:50:46 +0900

    create event

--- myapp/src/reducers/index.js
+++ myapp/src/reducers/index.js
@@ -2,8 +2,8 @@ const events = (state = [], action) => {
     switch(action.type){
         case 'CREATE_EVENT':
             const event = {title:action.title, body: action.body}
-            const lenght = state.length
-            let id = length === 0 ? id = 1 : id.state[length -1].id + 1
+            const length = state.length
+            const id = length === 0 ? 1 : state[length - 1].id + 1
             return [...state, {id:id,...event}]
         case 'DELETE_EVENT':
             return state
