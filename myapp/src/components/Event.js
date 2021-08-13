117734f2b4079bfb6609988aad9f05f9f94bbd00
KONDO, Hiroyasu <hrknd@users.noreply.github.com>
2021-08-13 12:18:42 +0900

    delete all events

--- myapp/src/components/Event.js
+++ myapp/src/components/Event.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const Event = ({dispatch, event}) =>{
+    const id = event.id
     const handleClickDeleteButton = () => {
-        dispatch({type:'DELETE_EVENT', id: event.id})
+        const result = window.confirm(`イベント(id=${id})を削除して良いですか`)
+        if (result) dispatch({type:'DELETE_EVENT', id})
     }
 
     return (
         <tr>
-        <td>{event.id}</td>
+        <td>{id}</td>
         <td>{event.title}</td>
         <td>{event.body}</td>
         <td><button type="button" className="btn btn-danger" onClick={handleClickDeleteButton}>削除</button></td>
